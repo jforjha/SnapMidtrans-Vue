321c0db63460bbd837ad3a217aff58381fa6220b
Abdul Salam <abdulsalam121196@gmail.com>
2021-02-19 23:37:24 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <button @click="pay()">Pay</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+const midtransClient = require('midtrans-client');
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    pay: () => {
+      // Create Snap API instance, empty config
+      let snap = new midtransClient.Snap();
+      
+      snap.apiConfig.set({
+        isProduction : false,
+        serverKey : 'YOUR_SERVER_KEY',
+        clientKey : 'YOUR_CLIENT_KEY'
+      });
+
+      // You don't have to re-set using all the options, 
+      // i.e. set serverKey only
+      snap.apiConfig.set({serverKey : 'YOUR_SERVER_KEY'});
+      console.log(snap);
+
+      // example parameter
+      let parameter = {
+        "transaction_details": {
+          "order_id": "test-transaction-123",
+          "gross_amount": 200000
+        }, "credit_card":{ "secure" : true }
+      };
+
+      snap.createTransaction(parameter)
+        .then((transaction) => {
+          // transaction token
+          let transactionToken = transaction.token;
+          console.log('transactionToken:',transactionToken);
+
+          // transaction redirect url
+          let transactionRedirectUrl = transaction.redirect_url;
+          console.log('transactionRedirectUrl:',transactionRedirectUrl);
+        })
+        .catch((e) => {
+          console.log('Error occured:',e.message);
+        });
+    }
   }
 }
 </script>
