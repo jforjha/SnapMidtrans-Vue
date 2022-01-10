6a9329e8a101647065faf2deea8e3484c2a02213
Gaurav Jha <gauravjha0907@fmail.com>
2022-01-10 12:45:52 +0530

    updated code

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@
 import HelloWorld from './components/HelloWorld.vue'
 import midtransClient from 'midtrans-client';
 // const midtransClient = require('midtrans-client');
-
+// exampple snaptoken="54ccfa1e-a290-4cbb-8c79-26855d38d762"
+var t2=''
 export default {
   name: 'App',
   components: {
@@ -18,42 +19,61 @@ export default {
   },
   methods: {
     pay: () => {
+            window.snap.pay(t2, {
+          // Optional
+          onSuccess: function(result){
+            /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          },
+          // Optional
+          onPending: function(result){
+            /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          },
+          // Optional
+          onError: function(result){
+            /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          }
+        });
       // Create Snap API instance, empty config
-      let snap = new midtransClient.Snap();
-      
-      snap.apiConfig.set({
-        isProduction : false,
-        serverKey : 'YOUR_SERVER_KEY',
-        clientKey : 'YOUR_CLIENT_KEY'
-      });
+    
 
       // You don't have to re-set using all the options, 
       // i.e. set serverKey only
-      snap.apiConfig.set({serverKey : 'YOUR_SERVER_KEY'});
-      console.log(snap);
+     
 
       // example parameter
-      let parameter = {
+    
+
+   
+    }
+  },
+  mounted(){
+  let snap = new midtransClient.Snap();
+      
+      snap.apiConfig.set({
+        isProduction : false,
+        serverKey : 'SB-Mid-server-NZEpgt7TfGWe675szo1VTyao',
+        clientKey : 'SB-Mid-client-WxSB3m3iWqKxhh8D'
+      });
+       snap.apiConfig.set({serverKey : 'SB-Mid-server-NZEpgt7TfGWe675szo1VTyao'});
+     let parameter = {
         "transaction_details": {
           "order_id": "test-transaction-123",
           "gross_amount": 200000
         }, "credit_card":{ "secure" : true }
       };
-
-      snap.createTransaction(parameter)
+         snap.createTransaction(parameter)
         .then((transaction) => {
           // transaction token
-          let transactionToken = transaction.token;
-          console.log('transactionToken:',transactionToken);
+         t2= transaction.token;
+          console.log('transactionToken:',t2);
 
           // transaction redirect url
-          let transactionRedirectUrl = transaction.redirect_url;
-          console.log('transactionRedirectUrl:',transactionRedirectUrl);
+          // let transactionRedirectUrl = transaction.redirect_url;
+          // console.log('transactionRedirectUrl:',transactionRedirectUrl);
         })
         .catch((e) => {
           console.log('Error occured:',e.message);
         });
-    }
   }
 }
 </script>
